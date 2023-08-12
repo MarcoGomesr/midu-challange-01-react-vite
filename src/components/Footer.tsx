cbe6cffc01f1a373926c624e3e6bea9cc88f84cd
marco <marcogomesr@gmail.com>
2023-08-12 12:00:31 +0200

    create components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="m-auto w-full max-w-7xl text-center">
+      Hecho con ♥ ️MarcoGomesr
+    </footer>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="m-auto w-full max-w-7xl p-4">
+      <nav>📚 Libros</nav>
+    </header>
+  )
+}

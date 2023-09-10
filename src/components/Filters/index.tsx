9ccd80a8ef9211741a021b24a024f29511976f78
marco <marcogomesr@gmail.com>
2023-09-10 20:21:50 +0200

    add space

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,7 +1,7 @@
 import Range from '../Range'
 import Select from '../Select'
 
-export default function Filters() {
+export default function Filters () {
   return (
     <div className="m-auto flex max-w-7xl flex-row items-start justify-start gap-5 pb-4 align-top">
       <Select />

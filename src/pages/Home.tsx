9399d01dc6f7c3e094236a6fdf096e3fcbac2762
marco <marcogomesr@gmail.com>
2023-09-10 14:57:00 +0200

    remove commented code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,58 +6,9 @@ import Header from '../layout/header'
 import { getBooks } from '../services/getBooks'
 import { useFilters } from '../utils/hooks/useFilters'
 
-export default function Home() {
+export default function Home () {
   const { filterBooks } = useFilters()
   const filteredBooks = filterBooks(getBooks)
-
-  // const genreInit: Book['genre'][] = Array.from(
-  //   new Set(books.map((book) => book.genre))
-  // )
-  // const [genreSelected, setGenreSelected] = useState<Book['genre']>('')
-  // const [readList, setReadList] = useState<Book[]>([])
-  // const [pagesFilter, setPagesFilter] = useState<Book['pages']>(0)
-  // const [filteredMatches, setFilteredMatches] = useState<Book[]>([])
-
-  // const handleBookClick = (bookISBN: Book['ISBN']) => {
-  //   console.log(filters)
-  //   const draft = readList.find((book) => book.ISBN === bookISBN)
-  //     ? readList.filter((book) => book.ISBN !== bookISBN)
-  //     : [...readList, matches.find((book) => book.ISBN === bookISBN)!]
-
-  //   setReadList(draft)
-  // }
-
-  // const matches = useMemo(() => {
-  //   if (genreSelected.length === 0) return books
-  //   return books.filter((book) => book.genre === genreSelected)
-  // }, [genreSelected])
-
-  // const updatedFilteredMatches = useMemo(() => {
-  //   return matches.filter((book) => book.pages <= pagesFilter)
-  // }, [matches, pagesFilter])
-
-  // useEffect(() => {
-  //   setFilteredMatches(updatedFilteredMatches)
-  // }, [matches, pagesFilter])
-
-  // const maxPages = useMemo(() => {
-  //   const pages = matches.map((book) => book.pages)
-
-  //   const pageCount = pages.reduce((prevPage, page) => {
-  //     return page > prevPage ? page : prevPage
-  //   })
-
-  //   setPagesFilter(pageCount)
-
-  //   return pageCount
-  // }, [matches])
-
-  // const minPages = useMemo(() => {
-  //   const pages = matches.map((book) => book.pages)
-  //   return pages.reduce((prevPage, page) => {
-  //     return page < prevPage ? page : prevPage
-  //   })
-  // }, [matches])
 
   return (
     <>
